48fd9b46c0facd567826e5855094112468bbc295
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2024-10-29 14:31:34 -0500

    commit

--- src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
+++ src/main/java/com/tecsup/petclinic/repositories/VetRepository.java
@@ -1,4 +1,26 @@
 package com.tecsup.petclinic.repositories;
 
-public class VetRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tecsup.petclinic.entities.Vet;
+
+/**
+ *
+ * @author
+ *
+ */
+@Repository
+public interface VetRepository extends JpaRepository<Vet, Integer> {
+
+    // Fetch vets by first name
+    List<Vet> findByFirstName(String firstName);
+
+    // Fetch vets by last name
+    List<Vet> findByLastName(String lastName);
+
+    @Override
+    List<Vet> findAll();
 }
